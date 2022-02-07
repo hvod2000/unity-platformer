d332098cf2551260d90b4850ee0d740c6190dfee
Uladzislau Khamkou <kham@tuta.io>
2022-02-08 01:13:34 +0300

    create script to follow target by camera

---
+++ Assets/CameraSoftController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSoftController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos = new Vector3(target.position.x, target.position.y, pos.z);
+        transform.position = pos;
+    }
+}
