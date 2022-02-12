6dd944cf966ad2e2cffa73ec426a7dff30b1a728
Uladzislau Khamkou <kham@tuta.io>
2022-02-12 13:43:58 +0300

    delete redundant code

--- Assets/InputControlledMovableCharacter.cs
+++ Assets/InputControlledMovableCharacter.cs
@@ -5,38 +5,23 @@ using UnityEngine;
 
 public class InputControlledMovableCharacter : MonoBehaviour
 {
-    private enum AnimationState
-    {
-        idle = 103,
-        running = 236,
-        falling = 7569,
-        dead = 1598,
-    }
-
     [SerializeField] private float speed = 4;
     [SerializeField] private LayerMask impassableObstacle;
-    private AnimationState state = AnimationState.idle;
 
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D collider;
-    private SpriteRenderer sprite;
     private Animator anim;
 
     private void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
-        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (state != AnimationState.dead)
-        {
-            ProcessInput();
-        }
-        UpdateAnimationState();
+        ProcessInput();
     }
 
     void ProcessInput()
@@ -56,38 +41,9 @@ public class InputControlledMovableCharacter : MonoBehaviour
         rigidbody2d.velocity = velocity;
     }
 
-    private void UpdateAnimationState()
-    {
-        if (state != AnimationState.dead)
-        {
-            state = AnimationState.idle;
-
-            if (Math.Abs(rigidbody2d.velocity.x) > 0.1)
-            {
-                sprite.flipX = (rigidbody2d.velocity.x < 0);
-                state = AnimationState.running;
-            }
-
-            if (rigidbody2d.velocity.y < -0.1)
-            {
-                state = AnimationState.falling;
-            }
-        }
-
-        anim.SetInteger("desired state", (int)state);
-    }
-
     private bool CanMoveInDirection(Vector2 direction)
     {
         return !Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, direction, 0.01f, impassableObstacle);
-    }
-
-    public void OnDeadlyRegionStay(GameObject regionGameObject)
-    {
-        if (rigidbody2d.velocity.y < -0.1)
-        {
-            state = AnimationState.dead;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
